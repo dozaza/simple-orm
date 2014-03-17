ceab9676f91520484720045fbd0f5094e86570e8
dozaza <baoan.feng@gmail.com>
2014-03-17 22:22:03 +0800

    add some base functions

---
+++ src/main/java/com/datayes/pms/simpleorm/database/Deleter.java
@@ -0,0 +1,16 @@
+package com.datayes.pms.simpleorm.database;
+
+import java.sql.Statement;
+
+/**
+ * Editor: intellij@Ubuntu
+ * User: baoan
+ * Date: 3/17/14
+ * Time: 10:18 PM
+ */
+public class Deleter {
+
+    public static void delete(Statement statement, String sql) {
+
+    }
+}
---
+++ src/main/java/com/datayes/pms/simpleorm/database/Updater.java
@@ -0,0 +1,17 @@
+package com.datayes.pms.simpleorm.database;
+
+import java.sql.Statement;
+
+/**
+ * Editor: intellij@Ubuntu
+ * User: baoan
+ * Date: 3/17/14
+ * Time: 10:19 PM
+ */
+public class Updater {
+
+    public static void update(Statement statement, String sql) {
+
+    }
+
+}
